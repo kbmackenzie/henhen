718bde1bb6d6e6db8964f9f6d6393f42bc00124f
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-05 17:28:05 -0300

    feat(environment): Add better Environment logic + data type

--- src/HenHen/Environment/Type.hs
+++ src/HenHen/Environment/Type.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE StrictData #-}
 
 module HenHen.Environment.Type
-( Environment(..)
-, getVariableMap
-, getEnvironment
+( Environment
+, createEnvironment
 ) where
 
 import HenHen.Config.Type (HenHenConfig(..), getInstaller)
@@ -11,29 +10,46 @@ import Control.Monad.IO.Class (MonadIO(..))
 import System.FilePath ((</>))
 import System.Directory (getCurrentDirectory)
 import System.Process (readProcess)
+import System.Environment (getEnvironment)
+import Data.Maybe (fromMaybe)
+import Data.List (unionBy)
+import Data.Function (on)
 import qualified Data.Text as Text
 
-data Environment = Environment
+type Environment = [(String, String)]
+
+data ChickenEnvironment = ChickenEnvironment
     { environmentRoot    :: FilePath
     , environmentRepo    :: FilePath
     , repositoryVariable :: String   }
-    deriving (Eq, Show)
 
-getVariableMap :: Environment -> [(String, String)]
-getVariableMap env =
+getChickenVars :: ChickenEnvironment -> [(String, String)]
+getChickenVars env =
     [ ("CHICKEN_INSTALL_PREFIX"    , environmentRoot env   )
     , ("CHICKEN_INSTALL_REPOSITORY", environmentRepo env   )
     , ("CHICKEN_REPOSITORY_PATH"   , repositoryVariable env) ]
 
-getEnvironment :: (MonadIO m) => HenHenConfig -> m Environment
-getEnvironment config = liftIO $ do
+getChickenEnvironment :: (MonadIO m) => HenHenConfig -> m ChickenEnvironment
+getChickenEnvironment config = liftIO $ do
     pwd <- getCurrentDirectory
     let localEnv = pwd </> ".chicken"
 
     let installer = getInstaller config
     systemRepo <- readProcess (Text.unpack installer) ["-repository"] mempty
 
-    return Environment
+    return ChickenEnvironment
         { environmentRoot    = localEnv
         , environmentRepo    = localEnv </> "lib" </> "chicken"
         , repositoryVariable = concat [ localEnv, ":", systemRepo ] }
+
+createEnvironment :: (MonadIO m) => HenHenConfig -> m Environment
+createEnvironment config = do
+    chickenEnv <- getChickenEnvironment config
+    parentEnv  <- liftIO getEnvironment
+    let pathVar = fromMaybe mempty $ lookup "PATH" parentEnv
+
+    let newPath    = concat [ environmentRoot chickenEnv </> "bin" , ":" , pathVar ]
+    let variables  = (:) ("PATH", newPath) $ getChickenVars chickenEnv
+    let processEnv = unionBy ((==) `on` fst) variables parentEnv
+
+    return processEnv
