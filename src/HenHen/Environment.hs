07ec6155859cb7138fa3a2701f0d9068b87075f7
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-08 13:06:26 -0300

    feat(environment): Add re-export module

---
+++ src/HenHen/Environment.hs
@@ -0,0 +1,10 @@
+module HenHen.Environment
+( Environment
+, createEnvironment
+, getLocalChicken
+, EnvironmentTask(..)
+, runEnvironmentTask
+) where
+
+import HenHen.Environment.Type (Environment, createEnvironment, getLocalChicken)
+import HenHen.Environment.Task (EnvironmentTask(..), runEnvironmentTask)
