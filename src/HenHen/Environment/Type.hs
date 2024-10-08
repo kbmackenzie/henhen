1f29fa531f9969dc04a6bcffc546a4744d836ca8
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-08 12:27:52 -0300

    feat(environment): When creating environment, handle errors with Packager

--- src/HenHen/Environment/Type.hs
+++ src/HenHen/Environment/Type.hs
@@ -7,6 +7,7 @@ module HenHen.Environment.Type
 ) where
 
 import HenHen.Config.Type (HenHenConfig(..), getInstaller)
+import HenHen.Packager (Packager, liftEither)
 import Data.List (unionBy)
 import Data.Function (on)
 import Data.Maybe (fromMaybe)
@@ -15,6 +16,7 @@ import System.FilePath ((</>))
 import System.Directory (getCurrentDirectory)
 import System.Process (readProcess)
 import System.Environment (getEnvironment)
+import Control.Exception (IOException, catch)
 
 type Environment = [(String, String)]
 
@@ -26,24 +28,31 @@ data ChickenEnvironment = ChickenEnvironment
 getLocalChicken :: (MonadIO m) => m FilePath
 getLocalChicken = (</> ".chicken") <$> liftIO getCurrentDirectory
 
+getSystemChicken :: (MonadIO m) => String -> m (Either String FilePath)
+getSystemChicken installer = liftIO $ do
+    let getter = readProcess installer ["-repository"] mempty
+    fmap Right getter `catch` \e -> do
+        let errorMessage = show (e :: IOException)
+        return (Left errorMessage)
+
 getChickenVars :: ChickenEnvironment -> [(String, String)]
 getChickenVars env =
     [ ("CHICKEN_INSTALL_PREFIX"    , environmentRoot env   )
     , ("CHICKEN_INSTALL_REPOSITORY", environmentRepo env   )
     , ("CHICKEN_REPOSITORY_PATH"   , repositoryVariable env) ]
 
-getChickenEnvironment :: (MonadIO m) => HenHenConfig -> m ChickenEnvironment
-getChickenEnvironment config = liftIO $ do
+getChickenEnvironment :: HenHenConfig -> Packager ChickenEnvironment
+getChickenEnvironment config = do
     localEnv <- getLocalChicken
     let installer = getInstaller config
-    systemRepo <- readProcess installer ["-repository"] mempty
+    systemRepo <- getSystemChicken installer >>= liftEither
 
     return ChickenEnvironment
         { environmentRoot    = localEnv
         , environmentRepo    = localEnv </> "lib" </> "chicken"
         , repositoryVariable = concat [ localEnv, ":", systemRepo ] }
 
-createEnvironment :: (MonadIO m) => HenHenConfig -> m Environment
+createEnvironment :: HenHenConfig -> Packager Environment
 createEnvironment config = do
     chickenEnv <- getChickenEnvironment config
     parentEnv  <- liftIO getEnvironment
