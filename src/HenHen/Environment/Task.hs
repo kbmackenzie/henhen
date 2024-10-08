2caf28f0947c94124ed7380e57392a0895ae8e0b
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-08 12:56:25 -0300

    feat(environment): Add EnvironmentTask data type + task logic

---
+++ src/HenHen/Environment/Task.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE LambdaCase #-}
+
+module HenHen.Environment.Task
+( EnvironmentTask(..)
+, runEnvironmentTask
+) where
+
+import HenHen.Packager (Packager, throwError)
+import HenHen.Environment.Type (Environment)
+import System.Process.Typed (proc, setEnv, setWorkingDir, runProcess, ExitCode(..))
+import Data.Maybe (fromMaybe)
+
+data EnvironmentTask = EnvironmentTask
+    { taskCommand     :: String
+    , taskArguments   :: [String]
+    , taskDirectory   :: Maybe FilePath
+    , taskErrorReport :: Maybe (String -> String) }
+
+runEnvironmentTask :: EnvironmentTask -> Environment -> Packager ()
+runEnvironmentTask task env = do
+    let setLocation = maybe id setWorkingDir (taskDirectory task)
+    let setters = setEnv env . setLocation
+    let process = setters $ proc (taskCommand task) (taskArguments task)
+
+    runProcess process >>= \case
+        ExitSuccess     -> return ()
+        (ExitFailure n) -> do
+            let report  = fromMaybe id (taskErrorReport task)
+            let command = unwords (taskCommand task : taskArguments task)
+            let message = concat ["process ", show command, " exited with code: ", show n]
+            (throwError . report) message
