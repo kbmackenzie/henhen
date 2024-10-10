921c480d51966bd3fd6a64b37290b054c8968070
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-10 01:08:37 -0300

    feat(actions): Generate EnvironmentTask values with build functions

--- src/HenHen/Actions/Build.hs
+++ src/HenHen/Actions/Build.hs
@@ -20,48 +20,52 @@ import HenHen.Environment
     , EnvironmentTask(..)
     , runEnvironmentTask
     )
-import System.FilePath ((</>), normalise, takeDirectory, replaceExtension)
+import System.FilePath ((</>), normalise, replaceExtension)
 import System.Directory (createDirectoryIfMissing)
 import Data.Maybe (fromMaybe)
 import Data.List (singleton)
 
-type BuildFn a = HenHenConfig -> Environment -> Meta -> a -> Packager ()
+type BuildFn a = HenHenConfig -> Meta -> a -> Packager EnvironmentTask
 
 buildFail :: String -> String -> String
 buildFail name message = concat
     [ "Couldn't build ", name, ": ", message ]
 
 buildModule :: BuildFn ModuleOptions
-buildModule config env meta options = do
-    localEnv <- getLocalChicken
+buildModule config meta options = do
+    buildDir <- (</> "build") <$> getLocalChicken
 
     let name      = (getKey . metaKey) meta
     let sourceDir = maybe id (flip (</>) . normalise) (configSources config)
     let source    = sourceDir $ fromMaybe (name ++ ".scm") (moduleSource options)
-    let buildDir  = localEnv </> "build" </> name
-    let output    = buildDir </> replaceExtension source "o"
-    let useList   = concatMap (("-uses" :) . singleton . getKey) (metaDeps meta)
-    let arguments =
-            [ "-c", "-J", source
-            , "-unit", name
-            , "-o"   , output    ]
-            ++ useList
-            ++ metaOptions meta
 
-    liftIO $ createDirectoryIfMissing True buildDir
+    let outputDir = buildDir </> name
+    let output    = outputDir </> replaceExtension source "o"
+
+    let uses      = concatMap (("-uses" :) . singleton . getKey) (metaDeps meta)
+    let includes  = map (("-I" ++) . (buildDir </>) . getKey) (metaDeps meta)
+    let arguments = concat
+            [ ["-c", "-J", source, "-unit", name, "-o", output]
+            , uses
+            , includes
+            , metaOptions meta ]
+
+    liftIO $ createDirectoryIfMissing True outputDir
     let compiler = getCompiler config
-    runEnvironmentTask env EnvironmentTask
+    return EnvironmentTask
         { taskCommand     = compiler
         , taskArguments   = arguments
         , taskDirectory   = Nothing
-        , taskErrorReport = Just . buildFail $ "module " ++ show name }
+        , taskErrorReport = Just . buildFail $ "module " ++ show name
+        , nextTask        = Nothing }
 
 buildEgg :: BuildFn EggOptions
-buildEgg config env meta options = do
+buildEgg config meta options = do
     let name      = (getKey . metaKey) meta
     let installer = getInstaller config
-    runEnvironmentTask env EnvironmentTask
+    return EnvironmentTask
         { taskCommand     = installer
         , taskArguments   = []
         , taskDirectory   = eggDirectory options
-        , taskErrorReport = Just . buildFail $ "egg " ++ show name }
+        , taskErrorReport = Just . buildFail $ "egg " ++ show name
+        , nextTask        = Nothing }
