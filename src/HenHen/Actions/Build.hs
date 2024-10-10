698161d31b1761bb70e401c84d00ab738540181d
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-10 02:15:26 -0300

    feat(actions): Generate build tasks in a pure way

--- src/HenHen/Actions/Build.hs
+++ src/HenHen/Actions/Build.hs
@@ -2,7 +2,6 @@ module HenHen.Actions.Build
 (
 ) where
 
-import HenHen.Packager (Packager, liftIO, catchError, throwError)
 import HenHen.Config
     ( HenHenConfig(..)
     , getCompiler
@@ -16,58 +15,61 @@ import HenHen.Config
     )
 import HenHen.Environment
     ( Environment
-    , getLocalChicken
+    , localChicken
     , EnvironmentTask(..)
     , runEnvironmentTask
     )
 import System.FilePath ((</>), normalise, replaceExtension)
 import System.Directory (createDirectoryIfMissing)
 import Data.Maybe (fromMaybe)
-import Data.List (singleton)
+import Data.List (singleton, delete)
 
-type BuildFn a = HenHenConfig -> Meta -> a -> Packager EnvironmentTask
+type GenerateTask a = HenHenConfig -> Meta -> a -> EnvironmentTask
 
-buildFail :: String -> String -> String
-buildFail name message = concat
-    [ "Couldn't build ", name, ": ", message ]
+getIncludes :: FilePath -> Meta -> [String]
+getIncludes root = map (("-I" ++) . (root </>) . getKey) . metaDeps
 
-buildModule :: BuildFn ModuleOptions
+getUses :: Meta -> [String]
+getUses = concatMap (("-uses" :) . singleton . getKey) . metaDeps
+
+buildModule :: GenerateTask ModuleOptions
 buildModule config meta options = do
-    buildDir <- (</> "build") <$> getLocalChicken
-
+    let buildDir  = localChicken </> "build"
     let name      = (getKey . metaKey) meta
-    let sourceDir = maybe id (flip (</>) . normalise) (configSources config)
+    let sourceDir = maybe id ((</>) . normalise) (configSources config)
     let source    = sourceDir $ fromMaybe (name ++ ".scm") (moduleSource options)
 
     let outputDir = buildDir </> name
     let output    = outputDir </> replaceExtension source "o"
 
-    let uses      = concatMap (("-uses" :) . singleton . getKey) (metaDeps meta)
-    let includes  = map (("-I" ++) . (buildDir </>) . getKey) (metaDeps meta)
     let arguments = concat
             [ ["-c", "-J", source, "-unit", name, "-o", output]
-            , uses
-            , includes
+            , getUses meta
+            , getIncludes buildDir meta
             , metaOptions meta ]
+    let preparations = createDirectoryIfMissing True outputDir
 
-    liftIO $ createDirectoryIfMissing True outputDir
     let compiler = getCompiler config
-    return EnvironmentTask
+    EnvironmentTask
         { taskCommand     = compiler
         , taskArguments   = arguments
         , taskDirectory   = Nothing
         , taskErrorReport = Just . buildFail $ "module " ++ show name
-        , taskPrepare     = Nothing
+        , taskPrepare     = Just preparations
         , nextTask        = Nothing }
 
-buildEgg :: BuildFn EggOptions
+buildEgg :: GenerateTask EggOptions
 buildEgg config meta options = do
     let name      = (getKey . metaKey) meta
     let installer = getInstaller config
-    return EnvironmentTask
+    EnvironmentTask
         { taskCommand     = installer
         , taskArguments   = []
         , taskDirectory   = eggDirectory options
         , taskErrorReport = Just . buildFail $ "egg " ++ show name
         , taskPrepare     = Nothing
         , nextTask        = Nothing }
+
+buildFail :: String -> String -> String
+buildFail name message = concat
+    [ "Couldn't build ", name, ": ", message ]
