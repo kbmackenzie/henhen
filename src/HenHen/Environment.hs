ba4228fb34658ade6f0a7283616ee5f2a468a257
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-10 03:00:40 -0300

    feat(environment): Add 'chickenBuild' path constant

--- src/HenHen/Environment.hs
+++ src/HenHen/Environment.hs
@@ -2,10 +2,11 @@ module HenHen.Environment
 ( Environment
 , createEnvironment
 , localChicken
+, chickenBuild
 , EnvironmentTask(..)
 , runEnvironmentTask
 ) where
 
-import HenHen.Environment.Folders (localChicken)
+import HenHen.Environment.Folders (localChicken, chickenBuild)
 import HenHen.Environment.Type (Environment, createEnvironment)
 import HenHen.Environment.Task (EnvironmentTask(..), runEnvironmentTask)
--- src/HenHen/Environment/Folders.hs
+++ src/HenHen/Environment/Folders.hs
@@ -1,6 +1,12 @@
 module HenHen.Environment.Folders
 ( localChicken
+, chickenBuild
 ) where
+
+import System.FilePath ((</>))
 
 localChicken :: FilePath
 localChicken = ".chicken"
+
+chickenBuild :: FilePath
+chickenBuild = localChicken </> "build"
