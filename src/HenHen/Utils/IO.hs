5fc0b9ed3e73e154eb326344181eb961daf5f7b4
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-11 04:20:11 -0300

    feat(utils): Add 'getFileModTime' IO util

--- src/HenHen/Utils/IO.hs
+++ src/HenHen/Utils/IO.hs
@@ -1,5 +1,6 @@
 module HenHen.Utils.IO
 ( readFileSafe
+, getFileModTime
 ) where
 
 import HenHen.Packager (Packager, liftIO, liftEither)
@@ -7,10 +8,23 @@ import Data.ByteString (ByteString)
 import Control.Exception (catch, IOException)
 import Control.Monad ((>=>))
 import qualified Data.ByteString as ByteString
+import System.Directory (getModificationTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+
+fileError :: String -> FilePath -> IOException -> String
+fileError message path err = concat [ message, " ", show path, ": ", show err ] 
 
 readFileSafe :: FilePath -> Packager ByteString
 readFileSafe path = (liftIO >=> liftEither) $ do
     let reader = fmap Right . ByteString.readFile
-    reader path `catch` \e -> do
-        let errMessage = concat [ "couldn't read file ", show path, ": ", show (e :: IOException) ]
-        return $ Left errMessage
+    reader path `catch` \err -> do
+        let message = fileError "couldn't read file" path err
+        return $ Left message
+
+getFileModTime :: FilePath -> Packager Integer
+getFileModTime path = (liftIO >=> liftEither) $ do
+    let getter = fmap Right . getModificationTime
+    utcTime <- getter path `catch` \err -> do
+        let message = fileError "couldn't get modification date for file" path err
+        return $ Left message
+    return $ fmap (round . utcTimeToPOSIXSeconds) utcTime
