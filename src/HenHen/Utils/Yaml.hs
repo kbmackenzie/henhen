2b397f08ebc617306dad26f922d0bb639e9bba2c
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-11 03:49:01 -0300

    feat(utils): Add YAML parsing utils

---
+++ src/HenHen/Utils/Yaml.hs
@@ -0,0 +1,16 @@
+module HenHen.Utils.Yaml
+( readYaml
+, prettyYaml
+) where
+
+import Data.Text (Text)
+import Data.ByteString (ByteString)
+import Data.Yaml (decodeEither', FromJSON, ToJSON, prettyPrintParseException)
+import Data.Yaml.Pretty (encodePretty, setConfCompare, defConfig)
+import Data.Bifunctor (first)
+
+readYaml :: (FromJSON a) => ByteString -> Either String a
+readYaml = first prettyPrintParseException . decodeEither'
+
+prettyYaml :: (ToJSON a) => (Text -> Text -> Ordering) -> a -> ByteString
+prettyYaml = encodePretty . flip setConfCompare defConfig
