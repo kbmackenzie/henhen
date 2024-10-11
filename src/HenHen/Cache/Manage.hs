82baaa0f97b0b59a0c536a411a8d8e3cc58d48a0
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-11 11:57:14 -0300

    feat(cache): Add cache creation logic

--- src/HenHen/Cache/Manage.hs
+++ src/HenHen/Cache/Manage.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE TupleSections #-}
+
 module HenHen.Cache.Manage
 ( cachePath
 , readCache
 , tryGetCache
 , writeCache
+, createCache
 ) where
 
 import HenHen.Packager (Packager, catchError, liftEither)
 import HenHen.Cache.Type (CacheData(..))
 import HenHen.Utils.Yaml (readYaml)
-import HenHen.Utils.IO (readFileSafe, writeFileSafe)
+import HenHen.Utils.IO (readFileSafe, getFileModTime, writeFileSafe)
+import HenHen.Utils.Time (getPosixTimeInSeconds)
 import HenHen.Environment (localChicken)
 import System.FilePath ((</>))
 import Data.Yaml (encode)
+import qualified Data.HashMap.Strict as HashMap
 
 cachePath :: FilePath
 cachePath = localChicken </> "build-info.json"
@@ -26,3 +31,14 @@ tryGetCache = fmap Just readCache `catchError` \_ -> return Nothing
 
 writeCache :: CacheData -> Packager ()
 writeCache = writeFileSafe cachePath . encode
+
+createCache :: [FilePath] -> Packager CacheData
+createCache files = do
+    let checkFile :: FilePath -> Packager (FilePath, Integer)
+        checkFile file = (file,) <$> getFileModTime file
+
+    buildTime_ <- getPosixTimeInSeconds
+    fileMap_   <- HashMap.fromList <$> mapM checkFile files
+    return CacheData
+        { buildTime = buildTime_
+        , fileMap   = fileMap_   }
