b72f5669d6a76847d0c7e4ef2c0265c3495aa406
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-11 12:22:15 -0300

    feat(utils): Add 'writeJson' util

--- src/HenHen/Utils/Json.hs
+++ src/HenHen/Utils/Json.hs
@@ -1,9 +1,13 @@
 module HenHen.Utils.Json
 ( readJson
+, writeJson
 ) where
 
-import Data.Aeson (FromJSON, eitherDecodeStrict)
-import Data.ByteString (ByteString)
+import Data.Aeson (FromJSON, ToJSON, encode, eitherDecodeStrict)
+import Data.ByteString (ByteString, toStrict)
 
 readJson :: (FromJSON a) => ByteString -> Either String a
 readJson = eitherDecodeStrict
+
+writeJson :: (ToJSON a) => a -> ByteString
+writeJson = toStrict . encode
