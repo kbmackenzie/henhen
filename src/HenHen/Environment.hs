54cf7721ab157d22bde180ac8ccbe7f8fd6d7514
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-12 00:01:40 -0300

    fix(environment): Correctly re-export constant

--- src/HenHen/Environment.hs
+++ src/HenHen/Environment.hs
@@ -2,11 +2,11 @@ module HenHen.Environment
 ( Environment
 , createEnvironment
 , localChicken
-, chickenBuild
+, localBuild
 , EnvironmentTask(..)
 , runEnvironmentTask
 ) where
 
-import HenHen.Environment.Folders (localChicken, chickenBuild)
+import HenHen.Environment.Folders (localChicken, localBuild)
 import HenHen.Environment.Type (Environment, createEnvironment)
 import HenHen.Environment.Task (EnvironmentTask(..), runEnvironmentTask)
