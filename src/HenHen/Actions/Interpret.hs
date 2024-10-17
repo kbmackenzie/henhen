e698569e3fbfd62afee759caa72aa783867f59d0
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-17 16:24:17 -0300

    feat(actions): Add better interpreter logic

--- src/HenHen/Actions/Interpret.hs
+++ src/HenHen/Actions/Interpret.hs
@@ -2,50 +2,18 @@ module HenHen.Actions.Interpret
 ( interpret
 ) where
 
-import HenHen.Environment
-    ( EnvironmentTask(..)
-    )
-import HenHen.Packager (throwError)
-import HenHen.Config
-    ( HenHenConfig(..)
-    , getInterpreter
-    , getSourcePath
-    , Target(..)
-    , Meta(..)
-    , MetaKey(..)
-    , SourceOptions(..)
-    )
-import qualified Data.HashMap.Strict as HashMap
-import System.FilePath (addExtension)
-import Data.Maybe (fromMaybe)
+import HenHen.Environment (EnvironmentTask(..))
+import HenHen.Config (HenHenConfig(..), getInterpreter)
 
-eggFail :: String
-eggFail = "HenHen cannot interpret eggs."
-
-getSource :: HenHenConfig -> Target -> Either String FilePath
-getSource config target = case target of
-    (Executable meta options) -> do
-        let name = (getKey . metaKey) meta
-        let path = fromMaybe (addExtension name "scm") (sourcePath options)
-        return . getSourcePath config $ path
-    (Egg meta _) -> let name = (getKey . metaKey) meta
-        in throwError . concat $ ["Cannot interpret target ", show name, ": ", eggFail]
-
-runInterpreter :: HenHenConfig -> FilePath -> EnvironmentTask
-runInterpreter config path = do
+interpret :: HenHenConfig -> FilePath -> EnvironmentTask
+interpret config source = do
     let interpreter = getInterpreter config
+    let failMessage :: String -> String
+        failMessage message = concat [ "Failure when running script ", show source, ": ", message ]
+
     EnvironmentTask
         { taskCommand     = interpreter
-        , taskArguments   = ["-s", path, "-b"]
+        , taskArguments   = ["-s", source, "-b"]
         , taskDirectory   = Nothing
-        , taskErrorReport = Nothing
+        , taskErrorReport = Just failMessage
         , afterTask       = Nothing }
-
-interpret :: HenHenConfig -> String -> Either String EnvironmentTask
-interpret config name = do
-    let maybeTarget = HashMap.lookup (MetaKey name) (configTargets config)
-    case maybeTarget of
-        (Just target) -> do
-            source <- getSource config target
-            return $ runInterpreter config source
-        Nothing -> throwError ("No target found with name " ++ show name)
