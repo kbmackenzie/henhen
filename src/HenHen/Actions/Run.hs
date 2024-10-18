819b6712cf47c8a358d2b48a078f1eb42b912cc8
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-18 02:03:57 -0300

    feat(actions): Add run logic

--- src/HenHen/Actions/Run.hs
+++ src/HenHen/Actions/Run.hs
@@ -1,13 +1,19 @@
 module HenHen.Actions.Run
-( runBinary
+( run
 ) where
 
+import HenHen.Packager (Packager, throwError)
+import HenHen.Config (HenHenConfig(..))
 import HenHen.Environment
-    ( EnvironmentTask(..)
+    ( Environment
     , localChickenBin
+    , EnvironmentTask(..)
+    , runEnvironmentTask
     )
 import HenHen.Utils.FilePath (toExecutablePath)
 import System.FilePath ((</>))
+import System.Process.Typed (shell, setEnv, ExitCode(..), runProcess)
+import qualified Data.HashMap.Strict as HashMap
 
 runBinary :: String -> EnvironmentTask
 runBinary name = do
@@ -21,3 +27,18 @@ runBinary name = do
         , taskDirectory   = Nothing
         , taskErrorReport = Just failMessage
         , afterTask       = Nothing }
+
+runScript :: Environment -> String -> String -> Packager ()
+runScript env name line = do
+    let process = setEnv env $ shell line
+    exitCode <- runProcess process
+    case exitCode of
+        ExitSuccess     -> return ()
+        (ExitFailure n) -> throwError . concat $ ["Script ", show name, " exited with code ", show n, "!"]
+
+run :: HenHenConfig -> Environment -> String -> Packager ()
+run config env name = do
+    let script = HashMap.lookup name (configScripts config)
+    case script of
+        (Just line) -> runScript env name line
+        Nothing     -> runEnvironmentTask env (runBinary name)
