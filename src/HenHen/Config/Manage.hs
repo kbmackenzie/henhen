fb8f4eb7ebe3c5e399346fc2716be7ffb76fa2ad
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-19 05:30:35 -0300

    feat(config): Add better error message when reading config file

--- src/HenHen/Config/Manage.hs
+++ src/HenHen/Config/Manage.hs
@@ -6,7 +6,7 @@ module HenHen.Config.Manage
 ) where
 
 import HenHen.Config.Type (HenHenConfig(..), configFieldOrder)
-import HenHen.Packager (Packager, liftEither)
+import HenHen.Packager (Packager, liftEither, throwError, catchError)
 import HenHen.Utils.IO (readFileSafe, writeFileSafe, exists, EntryType(..))
 import HenHen.Utils.Yaml (readYaml, prettyYaml)
 
@@ -15,7 +15,9 @@ configPath = "henhen.yaml"
 
 readConfig :: Packager HenHenConfig
 readConfig = do
-    content <- readFileSafe configPath
+    content <- readFileSafe configPath `catchError` \message -> do
+        let newMessage = "Couldn't read config file: " ++ message
+        throwError newMessage
     liftEither $ readYaml content
 
 writeConfig :: HenHenConfig -> Packager ()
