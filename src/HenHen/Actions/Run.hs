2d35ff3a715826d97dc011142c7ff9d220b98d14
kbmackenzie <kelly.a.betty@gmail.com>
2024-10-20 13:29:10 -0300

    feat(actions): When running non-existent binary, fail gracefully

--- src/HenHen/Actions/Run.hs
+++ src/HenHen/Actions/Run.hs
@@ -11,18 +11,25 @@ import HenHen.Environment
     , runEnvironmentTask
     )
 import HenHen.Utils.FilePath (toExecutablePath)
+import HenHen.Utils.IO (exists, EntryType(..))
 import System.FilePath ((</>))
 import System.Process.Typed (shell, setEnv, ExitCode(..), runProcess)
 import qualified Data.HashMap.Strict as HashMap
+import Control.Monad (unless)
 
-runBinary :: String -> [String] -> EnvironmentTask
+runBinary :: String -> [String] -> Packager EnvironmentTask
 runBinary name args = do
-    let bin = localChickenBin </> toExecutablePath name
+    let binary = localChickenBin </> toExecutablePath name
+
+    hasBinary <- exists File binary
+    unless hasBinary $
+        throwError ("Can't run non-existent binary: " ++ show binary)
+
     let failMessage :: String -> String
         failMessage message = concat ["Failure when running target ", show name, ": ", message]
 
-    EnvironmentTask
-        { taskCommand     = bin
+    return EnvironmentTask
+        { taskCommand     = binary
         , taskArguments   = args
         , taskDirectory   = Nothing
         , taskErrorReport = Just failMessage
@@ -41,4 +48,4 @@ run config env name args = do
     let script = HashMap.lookup name (configScripts config)
     case script of
         (Just line) -> runScript env name line
-        Nothing     -> runEnvironmentTask env (runBinary name args)
+        Nothing     -> runEnvironmentTask env =<< runBinary name args
