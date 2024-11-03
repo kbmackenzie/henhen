74dad7f8006a496090cbc5d03e46a00933cfdcc2
kbmackenzie <kelly.a.betty@gmail.com>
2024-11-03 10:50:38 -0300

    feat(logger): Add better log logic, support different log levels

--- src/HenHen/Logger.hs
+++ src/HenHen/Logger.hs
@@ -1,7 +1,12 @@
 module HenHen.Logger
-( logger
+( LogInfo(..)
+, LogType(..)
+, LogLevel(..)
+, logMessage
+, logVerbose
 ) where
 
+import Control.Monad (when, unless)
 import Control.Monad.IO.Class (MonadIO(..))
 import System.IO
     ( Handle
@@ -20,7 +25,15 @@ import System.Console.ANSI
     )
 
 data LogType = Info | Error | Warning
-    deriving (Show)
+    deriving (Eq, Show)
+
+data LogLevel = Quiet | Normal | Verbose
+    deriving (Eq, Show)
+
+data LogInfo = LogInfo
+    { logType  :: LogType
+    , logLevel :: LogLevel }
+    deriving (Eq, Show)
 
 printColor :: (MonadIO m) => Handle -> [SGR] -> String -> m ()
 printColor handle styles message = liftIO $ do
@@ -33,19 +46,26 @@ printColor handle styles message = liftIO $ do
         else do
             hPutStrLn handle message
 
-getColor :: LogType -> [SGR]
-getColor logType = case logType of
+getColor :: LogInfo -> [SGR]
+getColor logInfo = case logType logInfo of
     Info    -> []
     Error   -> [SetColor Foreground Vivid Red, SetConsoleIntensity BoldIntensity]
     Warning -> [SetColor Foreground Vivid Yellow]
 
-getHandle :: LogType -> Handle
-getHandle logType = case logType of
+getHandle :: LogInfo -> Handle
+getHandle logInfo = case logType logInfo of
     Info    -> stdout
     Error   -> stderr
     Warning -> stdout
 
-logger :: (MonadIO m) => LogType -> String -> m ()
-logger logType = printColor handle styles
-    where styles = getColor  logType
-          handle = getHandle logType
+logMessage :: (MonadIO m) => LogInfo -> String -> m ()
+logMessage logInfo = unless quiet . printColor handle styles
+    where styles = getColor  logInfo
+          handle = getHandle logInfo
+          quiet  = logLevel logInfo == Quiet
+
+logVerbose :: (MonadIO m) => LogInfo -> String -> m ()
+logVerbose logInfo = when verbose . printColor handle styles
+    where styles  = getColor  logInfo
+          handle  = getHandle logInfo
+          verbose = logLevel logInfo == Verbose
